7e7281a23afda96fb577d26fe5b652e458ab053d
unknown <o1673846326@gmail.com>
2025-04-01 16:54:47 +0700

    0104

---
+++ next-sitemap.ts
@@ -0,0 +1,7 @@
+const config = {
+    siteUrl: 'https://webhalong.id.vn', // Thay bằng URL của bạn
+    generateRobotsTxt: true, // Tạo file robots.txt tự động
+    // Các tùy chọn khác nếu cần
+};
+
+export default config;
